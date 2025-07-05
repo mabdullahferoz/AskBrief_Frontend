9aec7e3cecc1dcd124572771f7d6fa3f7bc6e5c7
Abdullah Feroz <00maferoz@gmail.com>
2025-07-05 16:42:12 +0500

    Started Frontend of AskBrief

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Sidebar from './components/Sidebar'
+
+const App = () => {
+  return (
+    <div className='hero w-screen h-screen overflow-clip bg-[#f8fafc]'>
+      
+    </div>
+  )
+}
+
+export default App
