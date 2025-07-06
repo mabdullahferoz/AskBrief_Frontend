bd5763e24762aecfd98ca5ea4666a3cf8a6b6932
Abdullah Feroz <00maferoz@gmail.com>
2025-07-06 12:53:50 +0500

    Created the File selection box

---
+++ src/components/UploadFile.jsx
@@ -0,0 +1,67 @@
+import React, { useState } from 'react'
+import { FaCloudUploadAlt } from 'react-icons/fa'
+import { ToastContainer, toast } from 'react-toastify'
+
+const UploadFile = () => {
+
+  const [file, setFile] = useState(null)
+  const [dragactive, setDragactive] = useState(false)
+  const allowedTypes = ["text/plain" , "application/pdf", "application/vnd.openxmlformats-officedocument.wordprocessingml.document"]
+
+  const handleClick = () => {
+    const fileInput = document.getElementById('upload-file');
+    fileInput.click();
+  }
+
+  const handleDragOver = (e) => {
+    e.preventDefault();
+    setDragactive(true);
+    e.dataTransfer.dropEffect = 'copy'; // Show copy cursor
+  }
+  const handleDragLeave = (e) => {
+    e.preventDefault();
+    setDragactive(false);
+  }
+  const handleDrop = (e) => {
+    e.preventDefault();
+    setDragactive(false);
+    const droppedFiles = e.dataTransfer.files;
+    if (droppedFiles.length > 0) {
+      const selectedFile = droppedFiles[0];
+      if (!allowedTypes.includes(selectedFile.type)) {
+        toast.error("Unsupported file type. Please upload a PDF, DOCX, or TXT file.");
+        return;
+      }
+      setFile(selectedFile);
+      console.log(selectedFile);
+    }
+  }
+
+  const handleFileChange = (e) => {
+    const selectedFile = e.target.files[0];
+    if (selectedFile){
+      setFile(selectedFile);
+      console.log(selectedFile);
+    }
+  }
+
+  return (
+    <div onDragOver={handleDragOver} onDragLeave={handleDragLeave} onDrop={handleDrop} onClick={handleClick} className={`drop-zone w-full h-full cursor-pointer border-dashed flex flex-col items-center justify-evenly p-2 border-2 ${file? "border-green-500 bg-green-50":"border-gray-300"} border-gray-300 text-gray-400 font-normal text-lg rounded-lg text-center `}>
+      <div className='flex flex-col items-center mt-1'>
+        <FaCloudUploadAlt color="#94a3b8" className='w-14 h-14' />
+        {
+          file? <span className='text-blue-600'>{file.name}</span> : <span>Drag & drop files here or click to browse</span>
+        }
+      </div>
+
+      <div className='flex flex-col items-center mb-1'>
+        <input type="file" id='upload-file' accept='.pdf,.docx,.txt' className='hidden' onChange={handleFileChange} />
+        <ToastContainer position='top-right' autoClose={3000} hideProgressBar={false} closeOnClick pauseOnHover theme='light' className={"text-sm"}/>
+        <label htmlFor="upload-file" className='bg-blue-600 cursor-pointer hover:bg-blue-500 transition-all p-2 rounded-lg font-medium mt-3 text-lg  text-white '>Browse Files</label>
+        <span className='text-sm mt-1'>Supported formats: PDF, DOCX, TXT</span>
+      </div>
+    </div>
+  )
+}
+
+export default UploadFile
--- src/components/Workarea.jsx
+++ src/components/Workarea.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react'
+import UploadFile from './UploadFile'
 
 const Workarea = () => {
     const [type, setType] = useState("doc")
@@ -8,19 +9,21 @@ const Workarea = () => {
                 <span className='logo text-blue-500'>AskBrief</span>
             </div>
             <div className='flex flex-col overflow-y-auto overflow-x-hidden items-center h-full px-5 '>
-                <div className="input-box h-80 w-4xl p-5 m-5 flex flex-col">
+                <div className="input-box h-80 w-4xl p-5 mt-5 flex flex-col">
                     <div className=" mx-5 flex text-sm font-semibold text-gray-400 border-b-1 border-gray-300">
-                        <span onClick={() => { setType("text") }} className={(type == "text" ? "border-b-2 px-10 py-2 border-blue-500 cursor-pointer" : "px-10 py-2 transition-all duration-75 hover:border-b-2 border-blue-500 cursor-pointer")}>Paste Text</span>
                         <span onClick={() => { setType("doc") }} className={(type == "text" ? "px-10 py-2 transition-all duration-75 hover:border-b-2 border-blue-500 cursor-pointer" : "px-10 py-2 border-b-2 border-blue-500 cursor-pointer") + 'px-10 py-2 hover:border-b-2 border-blue-500 cursor-pointer'}>Upload Documnet</span>
+                        <span onClick={() => { setType("text") }} className={(type == "text" ? "border-b-2 px-10 py-2 border-blue-500 cursor-pointer" : "px-10 py-2 transition-all duration-75 hover:border-b-2 border-blue-500 cursor-pointer")}>Paste Text</span>
                     </div>
                     <div className="input-field h-full w-full mt-5">
                         {type == "text" ? (
-                            <textarea className='w-full h-full box-border max-h-full min-h-full overflow-y-auto scrollbar-custom p-5 border-1 border-gray-300 bg-white rounded-lg text-lg font-normal' placeholder='Paste your text here...'></textarea>
+                            <textarea className='w-full h-full resize-none overflow-y-auto scrollbar-custom p-2 border-1 border-gray-300 bg-white rounded-lg text-lg font-normal' placeholder='Paste your text here...'></textarea>
                         ) : (
-                            <input type="file" className='w-full h-full p-3 border-2 border-gray-300 rounded-lg' />
+                            <UploadFile/>
                         )}
                     </div>
-                    <div className='text-sm text-white font-bold bg-blue-600 w-40 p-2 rounded-lg flex items-center justify-center cursor-pointer hover:bg-blue-400 mt-3 '>
+                </div>
+                <div className='btn w-4xl px-5'>
+                    <div className='text-sm text-white font-bold bg-blue-600 w-40 p-2 rounded-lg flex items-center justify-center cursor-pointer hover:bg-blue-500 mb-3 '>
                         Process Document
                     </div>
                 </div>
