b62fb249ca181e285fa072703f95ae315a953ee5
Abdullah Feroz <00maferoz@gmail.com>
2025-07-07 10:21:24 +0500

    Added React Router to route to the Login page

---
+++ src/components/Login.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Login = () => {
+  return (
+    <div className='h-screen w-screen flex items-center justify-center'>
+      <div className="form flex flex-col items-center gap-2 justify-evenly">
+
+      </div>
+    </div>
+  )
+}
+
+export default Login
--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,24 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import {
+  createBrowserRouter,
+  Router,
+  RouterProvider,
+} from "react-router";
+import Login from './components/login.jsx';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+  {
+    path: "/auth",
+    element: <Login />,
+  },
+])
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+  <RouterProvider router={router} />
 )
